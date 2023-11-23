a46f0b6aed701b1f1715819f039ad414c0d38e1a
jloveric <john.loverich@gmail.com>
2023-11-23 14:19:08 -0800

    Adding half space projection. Still need to test and verify this one

--- vector_neural_networks/layers.py
+++ vector_neural_networks/layers.py
@@ -15,8 +15,30 @@ class VectorLayer(torch.nn.Module) :
 
     def forward(self, x: Tensor):
         """
-        Input vector of size [basis x inputs_size X vector_size]
+        :param x: Input vector of size [batch x inputs_size X vector_size]
+        :returns: Vector of size [batch X output_size X vector_size]
         """
         assemble = torch.einsum("biv,voif->bfo", x, self.weight)
 
         return assemble
+    
+    class HalfSpaceProjection(torch.nn.Module):
+        """
+        Given a list of input vectors, project them onto the half space
+        """
+        def __init__(self, features:Tensor, vector_size:Tensor, epsilon:float=1e-6) :
+            self.weight = Parameter(torch.Tensor(features, vector_size))
+            self.weight.data.normal_(mean=0.0, std=1.0)
+            
+
+        def forward(self, x: Tensor) :
+            """
+            A bit different than the paper as they have an additional learned weight vector
+            that transforms x.
+            """
+            norm = torch.max(self.epsilon, torch.einsum('fv,fv->f', self.weight, self.weight))
+            projection = torch.einsum('bfv,fv->bf', x, self.weight)
+
+
+            result=torch.where(projection<0, x-(projection/norm)*(self.weight/norm), x)
+            return result
